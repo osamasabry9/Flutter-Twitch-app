b08f317a0dbe671eb1d5307c043efd8e7450871a
osamasabry9 <osamasabry324@gmail.com>
2022-10-06 14:28:37 +0200

    add go live screen

---
+++ lib/features/presentation/live/screen/go_live_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GoLiveScreen extends StatelessWidget {
+  const GoLiveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+     return const Center(
+      child:  Text('Live Screen'),
+    );
+  }
+}
