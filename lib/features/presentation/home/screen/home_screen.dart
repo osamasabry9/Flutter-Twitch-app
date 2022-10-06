9a3dfccabf4e16676ad2a4b49f0abe6beea28910
osamasabry9 <osamasabry324@gmail.com>
2022-10-06 14:27:50 +0200

    add main screen and home

--- lib/features/presentation/home/screen/home_screen.dart
+++ lib/features/presentation/home/screen/home_screen.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Home')),
+    return const Center(
+      child:  Text('Home Screen'),
     );
   }
 }
---
+++ lib/features/presentation/home/screen/main_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:twitch_clone/features/presentation/browser/screen/browser_screen.dart';
+import 'package:twitch_clone/features/presentation/home/screen/home_screen.dart';
+import 'package:twitch_clone/features/presentation/live/screen/go_live_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
+  int _selectedIndex = 0;
+  TabController? _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  onTapChange(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _tabController!.index = _selectedIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        controller: _tabController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          HomeScreen(),
+          GoLiveScreen(),
+          BrowserScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+              ),
+              label: 'Following'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_rounded,
+              ),
+              label: 'Go Live'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.copy,
+              ),
+              label: 'Browse'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: onTapChange,
+      ),
+    );
+  }
+}
