eb349b444917aa2b615e00f11cc5c649ba125ceb
osamasabry9 <osamasabry324@gmail.com>
2022-10-17 22:43:09 +0200

    firebase in web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:twitch_clone/app/helper/cache_helper.dart';
 import 'package:twitch_clone/app/utils/constants_manager.dart';
@@ -7,7 +8,20 @@ import 'app/my_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCoXG_O_nz8-94JHP4iHt9Y2sjjLYhJ-dA",
+          authDomain: "twitch-clone-522bd.firebaseapp.com",
+          projectId: "twitch-clone-522bd",
+          storageBucket: "twitch-clone-522bd.appspot.com",
+          messagingSenderId: "65331584605",
+          appId: "1:65331584605:web:903a94c3ebec6b32e577b4"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   await CacheHelper.init();
   AppConstants.uId = CacheHelper.getData(key: 'uId');
   runApp(const MyApp());
